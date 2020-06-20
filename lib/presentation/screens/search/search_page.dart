dc8b12ddd4f4e0bc7133ba19b38931bcf90487ba
Roman Levinzon <levinzon.roman@gmail.com>
2020-06-20 20:47:58 +0200

    Search scroolling

--- lib/presentation/screens/search/search_page.dart
+++ lib/presentation/screens/search/search_page.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:what_and_where/domain/models/movie.dart';
@@ -17,8 +14,6 @@ import 'package:what_and_where/presentation/translations/translations.dart';
 import 'package:what_and_where/utils/logger.dart';
 
 class SearchPage extends StatefulWidget {
-
-
   static Widget init(BuildContext context) {
     return BlocProvider<SearchBloc>(
       create: (_) => injector<SearchBloc>(),
@@ -31,77 +26,91 @@ class SearchPage extends StatefulWidget {
 }
 
 class SearchPageState extends State<SearchPage> {
-
   final ScrollController controller = ScrollController();
   SearchBloc bloc;
 
   @override
   void initState() {
-    controller.addListener(() { _onScroll(); });
+    controller.addListener(() {
+      _onScroll();
+    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SearchBloc, SearchState>(
-      builder: (context, state) => _buildContent(context, state)
-    );
+        builder: (context, state) => _buildContent(context, state));
   }
 
   Widget _buildContent(BuildContext context, SearchState state) {
     bloc = context.bloc();
-    logger.d("State $state");
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(top: 72,),
-              child: _buildListView(context, state)),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 12),
-            child: SearchBar(
-            onChanged: (query) => bloc.add(SearchQueryUpdated(query)),
+      body: CustomScrollView(
+        controller: controller,
+        slivers: <Widget>[
+          SliverPadding(
+            padding: const EdgeInsets.all(16.0),
+            sliver: SliverAppBar(
+              pinned: true,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+              title: SearchBar(
+                onChanged: (query) => bloc.add(SearchQueryUpdated(query)),
+              ),
             ),
           ),
+          SliverPadding(
+              padding: EdgeInsets.only(
+                top: 72,
+              ),
+              sliver: _buildListView(context, state)),
+        
         ],
       ),
     );
   }
 
-
   Widget _buildListView(BuildContext context, SearchState state) {
     if (state is ContentLoadedSuccess) {
-      return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount:3, childAspectRatio: 0.55, crossAxisSpacing: 10, mainAxisSpacing: 10),
-        controller: controller,
-        itemBuilder: (context, index) {
-          return index >= state.content.length ?
-          CenteredLoadingIndicator() :
-          VideoContentWidget(content: state.content[index]);
+      return SliverGrid(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 0.55,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        delegate: SliverChildBuilderDelegate((context, index) {
+          return index >= state.content.length
+              ? CenteredLoadingIndicator()
+              : VideoContentWidget(content: state.content[index]);
         },
-        itemCount: state.hasMore ? state.content.length + 1 : state.content.length,
-      );
-    } else if(state is Default) {
-      return EmptyPageHolder(
-        title: Translation.searchHint,
-        subtitle: Translation.searchHintSubtitle,
-        image: Icon(Icons.search, size: 66,)
+        childCount:
+        state.hasMore ? state.content.length + 1 : state.content.length,
+      ));
+    } else if (state is Default) {
+      return SliverFillRemaining(
+        child: EmptyPageHolder(
+            title: Translation.searchHint,
+            subtitle: Translation.searchHintSubtitle,
+            image: Icon(
+              Icons.search,
+              size: 66,
+            )),
       );
     } else {
-      return CenteredLoadingIndicator();
+      return SliverFillRemaining(
+        child: CenteredLoadingIndicator(),
+      );
     }
   }
-
 
   void _onScroll() {
     final maxScroll = controller.position.maxScrollExtent;
     final currentScroll = controller.position.pixels;
 
     if (maxScroll - currentScroll <= 200.0) {
-      if (!bloc.isLoadingNextPage && bloc.hasMore)  {
+      if (!bloc.isLoadingNextPage && bloc.hasMore) {
         bloc.add(LoadNext());
       }
     }
   }
-
 }
